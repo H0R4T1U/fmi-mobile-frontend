a0b2d6dad1ea4d7039e728a4d384fbecae6535b4
Baroană Alexandru-Cătălin <baroanacatalin04@gmail.com>
2025-03-26 19:16:34 +0200

    added profile page containers implementation

--- app/tabs/ProfileScreen.js
+++ app/tabs/ProfileScreen.js
@@ -1,10 +1,18 @@
 import FloatingHeader from "../components/FloatingHeader";
 import React from "react";
+import {View} from "react-native";
+import ProfilePageSmallContainer from "../components/ProfilePageSmallContainer";
+import {scaleHeight} from "../utils/ScaleFunction";
+import ProfilePageLargeContainer from "../components/ProfilePageLargeContainer";
 
 export default function ProfileScreen() {
     return (
-        <>
+        <View style={{alignItems: "center"}}>
             <FloatingHeader text="PROFIL"/>
-        </>
+            <ProfilePageSmallContainer title="STUDENT" content="PLACEHOLDER"/>
+            <ProfilePageLargeContainer title="CREDENȚIALE" username="PLACEHOLDER" password="PLACEHOLDER"/>
+            <ProfilePageSmallContainer title="NR. MATRICOL" content="PLACEHOLDER"/>
+            <ProfilePageSmallContainer title="COD" content="PLACEHOLDER"/>
+        </View>
     )
 }
